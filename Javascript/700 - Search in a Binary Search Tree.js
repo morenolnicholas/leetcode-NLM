b0607bf701cdb6f888ce654269321911fb7f8958
Nicholas Moreno <nicholaslonggeno.moreno@calbaptist.edu>
2020-06-21 13:08:21 -0700

    LeetCode Problem 700 completed.

---
+++ Javascript/700 - Search in a Binary Search Tree.js
@@ -0,0 +1,28 @@
+/**
+Given the root node of a binary search tree (BST) and a value.
+You need to find the node in the BST that the node's value equals the given value.
+Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+
+function dfs(root){
+    if(!root) return null;
+    if(root.val == val){
+        return root;
+    }
+    return (val < root.val) ? dfs(root.left): dfs(root.right);
+}
+
+return dfs(root)
+};
